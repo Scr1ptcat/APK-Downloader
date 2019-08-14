5894db51ab781e1eeaf988da2fb00fd25d1bdd04
sabatu <sabatu@gmail.com>
2019-08-13 17:45:13 -0700

    *Finished implementation of basic apk downloader.. tested as working.

--- APK Downloader/Program.fs
+++ APK Downloader/Program.fs
@@ -10,23 +10,24 @@ module Configuration =
     let apiKey = "b1207f182ed1c3b28f8cc12ad330bb4079e38dbcab85828bed36c22dbbe0a078"
     let androzooURI = "https://androzoo.uni.lu/api/download?apikey={0}&sha256={1}"
     let inputDirectory = @"C:\Users\John\Desktop\multi-versioned-apks.txt"
-    let outputDirectory = @"C:\test.apk"
+    let outputDirectory = @"C:\APKs\"
 
-let ReadTargetSHAs () (shaList : string []) =
+let ReadTargetSHAs shaList : string [] =
 
     File.ReadAllLines(Configuration.inputDirectory)
 
-let ProcessInputFile(shaList : string []) =
+let ProcessInputFile (shaList : string [])  =
 
-    for apkMetadata in shaList do
+    let apkMetadataList = [for apkMetadata in shaList do yield apkMetadata.Split [|' '|] ]    
 
-        apkMetadata.
+    apkMetadataList
 
-let DownloadAPK(sha256 : string) (downloadedAPK: byte []) =
+let DownloadAPK(sha256 : string) : byte [] =
         
     let apkDownloader = new WebClient()
     
     let downloadedAPK =
+
         try
             apkDownloader.DownloadData(String.Format(Configuration.androzooURI, 
                                                     Configuration.apiKey,
@@ -36,12 +37,17 @@ let DownloadAPK(sha256 : string) (downloadedAPK: byte []) =
     
     downloadedAPK
     
-let WriteAPK(downloadedAPK : byte []) =
+let WriteAPK downloadedAPK name version =
     
-    File.WriteAllBytes(Configuration.outputDirectory , downloadedAPK)
+    File.WriteAllBytes(Configuration.outputDirectory + name + "-" + version + ".apk", downloadedAPK)
 
 [<EntryPoint>]
 let main argv =   
+
+    let shaList = ReadTargetSHAs()
+    let apkList = ProcessInputFile(shaList) 
+
+    apkList |> List.iter (fun x -> WriteAPK (DownloadAPK(x.[0])) x.[1] x.[2])  
 
     0
 
